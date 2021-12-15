13dc9d5e0444984a28d03811394c622b7cc088dc
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-12-16 00:23:57 +0900

    create guess-number-game

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
+const util = require('util');
 const readline = require('readline');
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const main = async () => {
-  for (;;) {
-    const answer = await prompt('');
-    if ([].includes(answer)) {
-      break;
-    } else if ([].includes(answer)) {
-    }
+const print = function (message) {
+  process.stdout.write(util.format.apply(this, arguments));
+};
+
+const answer = Math.floor(Math.random() * 100) + 1;
+let turn = 0;
+
+print('数当てゲーム★\n');
+print('1 ~ 100の間の数字を入力してください');
+
+readlineInterface.on('line', (line) => {
+  ++turn;
+
+  let inputNumber = +line;
+  if (global.isNaN(inputNumber)) {
+    print('不正な値です\n');
+  } else if (inputNumber < answer) {
+    print('もっと大きい！\n');
+  } else if (inputNumber > answer) {
+    print('もっと小さい\n');
+  } else {
+    print('正解!!\n');
+    readlineInterface.close();
+    return;
   }
-  console.log('');
-};
-
-// let user enter value
-const prompt = async (msg) => {
-  console.log(msg);
-  const answer = await question('>');
-  return answer.trim();
-};
-
-// get standard input
-const question = (question) => {
-  const readlineInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  return new Promise((resolve) => {
-    readlineInterface.question(question, (answer) => {
-      resolve(answer);
-      readlineInterface.close();
-    });
-  });
-};
-
-(async () => {
-  await main();
-})();
+  print('1 ~ 100の間の数字を入力してください');
+});
+readlineInterface.on('close', () => {
+  print('答えは %d, 正解までに %d ターンかかりました. \n', answer, turn);
+});
